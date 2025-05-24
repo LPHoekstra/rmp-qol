b3d1dc224565dd950fa3e1bddbe709ea876d4ca0
MalBourbie <MalBourbie@gmail.com>
2025-05-24 14:15:31 +0200

    refactoring AutoHarvest

--- src/client/java/com/rmpqol/AutoHarvest.java
+++ src/client/java/com/rmpqol/AutoHarvest.java
@@ -1,20 +1,34 @@
 package com.rmpqol;
 
 import net.fabricmc.fabric.api.event.client.player.ClientPlayerBlockBreakEvents;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CropBlock;
 import net.minecraft.client.MinecraftClient;
 
 public class AutoHarvest {
-    private MinecraftClient client;
+    private static MinecraftClient client = MinecraftClient.getInstance();
+    private static boolean eventRegistered = false;
+    private static boolean isHarvesting = false;
 
-    public AutoHarvest(MinecraftClient client) {
-        this.client = client;
-    }
-
-    public void harvest() {
+    private static void harvest() {
+        isHarvesting = true;
         client.options.attackKey.setPressed(true);
 
-        ClientPlayerBlockBreakEvents.AFTER.register((world, player, pos, state) -> {
-            client.options.attackKey.setPressed(false);
-        });
+        if (!eventRegistered) {
+            ClientPlayerBlockBreakEvents.AFTER.register((world, player, pos, state) -> {
+                client.options.attackKey.setPressed(false);
+                isHarvesting = false;
+            });
+            eventRegistered = true;
+        }
+    }
+
+    public static void handleAutoHarvestBlockCallback(Block block, BlockState blockState) {
+        if (block instanceof CropBlock cropBlock && cropBlock.getAge(blockState) == cropBlock.getMaxAge()) {
+            if (!isHarvesting) {
+                harvest();
+            }
+        }
     }
 }
--- src/client/java/com/rmpqol/EventListener.java
+++ src/client/java/com/rmpqol/EventListener.java
@@ -4,13 +4,10 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.CropBlock;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 
 public class EventListener {
-    private static MinecraftClient client = MinecraftClient.getInstance();
 
     private static void blockCallback() {
         UseBlockCallback.EVENT.register((playerEntity, world, hand, blockHitResult) -> {
@@ -19,10 +16,7 @@ public class EventListener {
             Block block = blockState.getBlock();
 
             // Autoharvest
-            if (block instanceof CropBlock cropBlock && cropBlock.getAge(blockState) == cropBlock.getMaxAge()) {
-                AutoHarvest autoHarvest = new AutoHarvest(client);
-                autoHarvest.harvest();
-            }
+            AutoHarvest.handleAutoHarvestBlockCallback(block, blockState);
 
             return ActionResult.PASS;
         });
